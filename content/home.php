691034e9791f6f05bc4936d7c5056257cbd0f42d
Hugo M <hugo@hugomassarri.com>
2015-04-28 08:01:36 -0400

    First commit

---
+++ content/home.php
@@ -0,0 +1,19 @@
+<?php
+	/**
+	*	home.php
+	*
+	*	Content for the Home page
+	*
+	*	@version	1.2 2015-0427
+	*	@package	Smithside Auctions
+	*	@copyright	Copyright 2015 Smithside Auctions
+	*	@license	GNU General Public License
+	*	@since		Since Release 1.0
+	**/
+?>
+		<h1>Next Auction September 22nd</h1>
+        <p>Join us for our next auction of historic clothing
+         to be held at the St. Paul's Auditorium in NYC on September 22nd at 1 o'clock.</p>
+        
+        <p>Lots can be viewed the prior day from 4pm until 7pm and again on Thursday morning from
+         10am to noon.</p>
